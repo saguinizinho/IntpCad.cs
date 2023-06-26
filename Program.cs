169b6486827e340a1b8056b3e3839c01d933c130
CAIO SAGUINI <92821616+saguinizinho@users.noreply.github.com>
2023-06-26 17:32:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string userName = "<Caio>";
+string dateString = DateTime.Today.ToShortDateString();
+
+// Use the + and += operators for one-time concatenations.
+string str = "Hello " + userName + ". Today is " + dateString + ".";
+System.Console.WriteLine(str);
+
+str += " How are you today?";
+System.Console.WriteLine(str);
+
+str += " How are the projects going?";
+System.Console.WriteLine(str);
